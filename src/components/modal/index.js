8f5045cf1d91f520792cec9cc846f24b350dbd85
Marinho <alesandra.i.marinho@accenture.com>
2023-12-25 17:50:43 -0300

    Logic of handle copy password and close modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,13 +1,23 @@
 import { View, StyleSheet, Text, Pressable, TouchableOpacity } from "react-native";
+import * as Clipboard from 'expo-clipboard'
 
 export function ModalPassword({password, handleClose}){
+
+    async function handleCopyPassword() {
+        await Clipboard.setStringAsync(password)
+        alert('Senha salva com sucesso!')
+
+        handleClose();
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.title}>
                     Senha gerada
                 </Text>
-                <Pressable style={styles.innerPassword}>
+
+                <Pressable style={styles.innerPassword} onLongPress={handleCopyPassword}>
                     <Text style={styles.textPassword}>{password}</Text>
                 </Pressable>
 
